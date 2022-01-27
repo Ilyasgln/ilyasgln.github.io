b2c3416a755d0e1226a56d5784e78415df519128
adrianbaecker <98322297+adrianbaecker@users.noreply.github.com>
2022-01-27 10:20:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,30 +13,32 @@ let text = ' { "match_results":[ ' +
 const resultJSON = JSON.parse(text);  
 
 function kreiere() {
-    let example_date = resultJSON.match_results[0].date;
-    let example_matchup = "FC Bayern vs FC Köln";
-    let example_result = "4:0";
+    for(let i = 0; i < resultJSON.length; i++){
+      let example_date = resultJSON.match_results[i].date;
+      let example_matchup = resultJSON.match_results[i].matchup;
+      let example_result = resultJSON.match_results[i].result;
     
-    const match = document.createElement("p");
-    match.id = "match";
-    document.getElementById("table").appendChild(match);
-    
-    const match_element = document.getElementById("match");
-    
-    const date = document.createElement("span");
-    date.innerText = example_date + " ";
-    match_element.appendChild(date); 
-   
-    
-    const matchup = document.createElement("span");
-    matchup.innerText = example_matchup + " ";
-    match_element.appendChild(matchup);
-    
-    const result = document.createElement("span");
-    result.innerText = example_result;
-    match_element.appendChild(result);
-    
-    const br = document.createElement("br");
-    match_element.appendChild(br);
+      const match = document.createElement("p");
+      match.id = "match";
+      document.getElementById("table").appendChild(match);
+
+      const match_element = document.getElementById("match");
+
+      const date = document.createElement("span");
+      date.innerText = example_date + " ";
+      match_element.appendChild(date); 
+
+
+      const matchup = document.createElement("span");
+      matchup.innerText = example_matchup + " ";
+      match_element.appendChild(matchup);
+
+      const result = document.createElement("span");
+      result.innerText = example_result;
+      match_element.appendChild(result);
+
+      const br = document.createElement("br");
+      match_element.appendChild(br);
+    }
 
   }
